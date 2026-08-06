5b71abd039d46e3bf71175a311d3dd9cbb92ba84
A19AlbertoNS <a19albertons@iessanclemente.net>
2026-08-06 19:52:58 +0200

    Cambios para introducir la IA a la segunda pala

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+/// <summary>
+/// Clase que gestiona los ajustes del juego, como si el jugador 2 es humano o no.
+/// </summary>
+public class Settings : MonoBehaviour
+{
+    private bool isPlayer2Human;
+
+    public static Settings instance;
+
+    /// <summary>
+    /// Al ser levantada la instancia de la clase comprueba si el jugador 2 es humano o no y lo guarda en la variable isPlayer2Human
+    /// </summary>
+    void Start()
+    {
+        // Deshabilitado en caso de que no exista la clave y comprobamos esta variable al arrancar el juego para saber si el jugador 2 es humano o no.
+        isPlayer2Human = PlayerPrefs.GetString("isPlayer2Human", "false") == "true";
+    }
+
+    /// <summary>
+    /// Devuelve si el jugador 2 es humano o no.
+    /// </summary>
+    /// <returns>Devuelve si el jugador 2 es humano o no</returns>
+    public bool getIsPlayer2Human()
+    {
+        // Devuelve si el jugador 2 es humano o no.
+        return isPlayer2Human;
+    }
+
+    /// <summary>
+    /// Patron singleton para asegurar una unica instancia de los ajustes de usuario en todo el ciclo de vida del juego
+    /// </summary>
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
